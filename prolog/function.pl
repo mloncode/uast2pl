62350b39d77ed3780e255fd2ab0e5de14bafc40b
kuba-- <kuba@sourced.tech>
2019-10-21 19:40:21 +0200

    renaming

--- prolog/function.pl
+++ prolog/function.pl
@@ -12,7 +12,7 @@ extract_name_(_, _, []) :- !.
 % extract_fntype_arg_ is a private predicate
 % It just extracts function arguments.
 extract_fntype_arg_([], []) :- !.
-extract_fntype_arg_([['uast:FunctionType', Args, _, _] | TailList], JoinedList) :- \+ var(Args), extract_fntype_arg_(TailList, List), join_(Args, List, JoinedList), !.
+extract_fntype_arg_([['uast:FunctionType', Args, _, _] | TailList], AllArgs) :- \+ var(Args), extract_fntype_arg_(TailList, List), join_(Args, List, AllArgs), !.
 extract_fntype_arg_([Head | Tail], List) :- extract_fntype_arg_(Head, HeadList), extract_fntype_arg_(Tail, TailList), join_(HeadList, TailList, List), !.
 extract_fntype_arg_(_, []):- !.
 
@@ -20,6 +20,6 @@ extract_fn_name_(FnGroup, Name) :- extract_name_('uast:Alias', FnGroup, Name).
 extract_fn_args_(FnGroup, Args) :- extract_fntype_arg_(FnGroup, FnArgs), extract_name_('uast:Argument', FnArgs, Args).
 
 
-% function_def extracts function name and function arguments for each function_group.
+% function_arg extracts function name and function arguments for each function_group.
 % It returns Fn as a atomic value and Args as a list of names.
 function_arg(Fn, Args) :- function_group(FnGroup), extract_fn_name_(FnGroup, [Fn]), extract_fn_args_(FnGroup, Args).
